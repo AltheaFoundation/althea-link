884dc94792a10afeb751d8101d3bed4bda4669cd
Justin Kilpatrick <justin@althea.net>
2024-11-20 20:19:15 -0500

    fixup! add apr

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -1,11 +1,9 @@
 use std::sync::Arc;
 
 use crate::althea::endpoints::{
-
-    get_delegations, get_proposals, get_staking_info, get_validators, query_all_burn_ranged,
-    query_all_init_pools, query_all_mint_ambient, query_all_mint_ranged, query_pool,
-    user_pool_positions, user_positions,
-
+    get_delegations, get_proposals, get_staking_info, get_validators, pool_liq_curve, pool_stats,
+    query_all_burn_ambient, query_all_burn_ranged, query_all_init_pools, query_all_mint_ambient,
+    query_all_mint_ranged, query_pool, user_pool_positions, user_positions,
 };
 use crate::tls::{load_certs, load_private_key};
 use crate::Opts;
@@ -47,7 +45,6 @@ pub async fn start_server(opts: Opts, db: Arc<rocksdb::DB>) {
             .service(get_proposals)
             .service(get_delegations)
             .service(get_staking_info)
-
             // Debug endpoints
             .service(
                 web::scope("/debug")
@@ -58,7 +55,6 @@ pub async fn start_server(opts: Opts, db: Arc<rocksdb::DB>) {
                     .service(query_all_mint_ambient)
                     .service(query_all_burn_ambient),
             )
-
             // Graphcache-go endpoints
             .service(
                 web::scope("/gcgo")
