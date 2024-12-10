2ee7f7dbead9290e7fa52e025b2238660e877a17
Christian Borst <christianoborst@gmail.com>
2024-12-10 15:14:35 -0500

    Use correct trade endpoint

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -3,8 +3,8 @@ use std::sync::Arc;
 use crate::althea::endpoints::ambient::{
     moralis_eth_in_usdc, pool_liq_curve, pool_stats, query_all_burn_ambient,
     query_all_burn_knockout, query_all_burn_ranged, query_all_init_pools, query_all_mint_ambient,
-    query_all_mint_knockout, query_all_mint_ranged, query_pool, slingshot_trade_get,
-    user_pool_positions, user_positions,
+    query_all_mint_knockout, query_all_mint_ranged, query_pool, slingshot_trade,
+    slingshot_trade_get, user_pool_positions, user_positions,
 };
 use crate::althea::endpoints::cosmos::{
     get_delegations, get_proposals, get_staking_info, get_validators,
@@ -79,11 +79,7 @@ pub async fn start_server(opts: Opts, db: Arc<rocksdb::DB>) {
             .service(
                 web::scope("/api")
                     // Slingshot Trade endpoint
-                    .service(
-                        web::scope("/v3")
-                            // .service(slingshot_trade)
-                            .service(slingshot_trade_get),
-                    )
+                    .service(web::scope("/v3").service(slingshot_trade))
                     // Moralis price endpoint
                     .service(web::scope("/v2.2").service(moralis_eth_in_usdc)),
             )
